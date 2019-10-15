f9d26c7d92f08ec6efd36e18fc74dcd02703c096
BSLeto <55754084+BSLeto@users.noreply.github.com>
2019-10-15 19:27:52 +0300

    Add files via upload

---
+++ libs/main.js
@@ -0,0 +1,5 @@
+const live = document.querySelector('.score'),
+    start = document.querySelector('.start'),
+    gameArea = document.querySelector('.gameArea'),
+    player = document.createElement('div');
+
